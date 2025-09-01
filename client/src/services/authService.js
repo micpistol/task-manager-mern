f095b517b5e28d7b42dd305805aa358b0e96cc90
micpistol <m.pistorio@gmail.com>
2025-09-01 13:46:32 -0700

    Hardcode API URL for testing

--- client/src/services/authService.js
+++ client/src/services/authService.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:5000/api';
+const API_URL = 'https://task-manager-backend-z0fh.onrender.com/api';
 
 // Create axios instance
 const api = axios.create({
